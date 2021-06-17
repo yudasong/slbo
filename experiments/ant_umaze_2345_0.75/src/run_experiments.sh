b9e08a6aa02c1f955e46ef7e1841f3b078afca16
yudasong <anepycn@gmail.com>
2021-06-16 23:35:41 -0400

    .

--- experiments/ant_umaze_2345_0.75/src/run_experiments.sh
+++ experiments/ant_umaze_2345_0.75/src/run_experiments.sh
@@ -5,6 +5,6 @@ for env_name in $1; do
     #for random_seed in 1234 2314 2345 1235; do
     for random_seed in $2; do
         python main.py -c configs/algos/slbo_bm_1m.yml configs/env_tingwu/${env_name}.yml \
-	    -s ckpt.model_load=./experiments/${env_name}_${random_seed}/stage-70.npy seed=${random_seed} ckpt.render=True
+	    -s log_dir=./experiments/${env_name}_${random_seed}_$3 seed=${random_seed} pc.bonus_scale=$3
     done
 done
